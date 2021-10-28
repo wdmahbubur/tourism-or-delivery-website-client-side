e014ce818b4831d46ea2dac6505aab113917c0e0
Mahbubur Rahman <mrrohanc@gmail.com>
2021-10-29 03:31:43 +0600

    order page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import Footer from './components/Footer/Footer';
+import Order from './components/pages/Order/Order';
 function App() {
   return (
     <div>
@@ -12,6 +13,9 @@ function App() {
         <Switch>
           <Route exact path='/'>
             <Home></Home>
+          </Route>
+          <Route path='/order'>
+            <Order></Order>
           </Route>
         </Switch>
         <Footer></Footer>
